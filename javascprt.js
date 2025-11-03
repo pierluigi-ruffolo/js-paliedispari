653dc0bd8ac7b1ce893701d56a8e117d06441b0c
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-11-03 13:06:13 +0100

    first commit

---
+++ javascprt.js
@@ -0,0 +1,17 @@
+/* Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma 
+(NON usare split, reverse e join) */
+
+// chiediamo all'utente una parola
+// creiamo una funzione per capire se la parola è palindroma
+// parametro: parola inserita dall'utente
+// return: String;
+
+/* 
+Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto. */
